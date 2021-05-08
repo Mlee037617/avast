6e778dd6bea30f9ac4048354e6d06dd7fa97b6fc
Mlee037617 <83857856+Mlee037617@users.noreply.github.com>
2021-05-08 15:04:40 +0800

    Add files via upload

--- tasks2/robot/vars/var.py
+++ tasks2/robot/vars/var.py
@@ -1,1 +1,34 @@
+base_url = {
+	'demoqa':'https://demoqa.com/automation-practice-form'
+}
 
+ids = {
+	'radios':{
+		'gender':{
+			'male':'gender-radio-1',
+			'female':'gender-radio-2',
+			'other':'gender-radio-3'
+		}
+	}
+}
+
+xpaths = {
+	'required_fields':{
+		'texts':{
+			'first_name':'//*[@id="firstName"]',
+			'last_name':'//*[@id="lastName"]',
+		},
+		'nums':{
+			'mobile':'//*[@id="userNumber"]',
+		}		
+	}
+}
+
+texts = {
+	'home_page':{
+		'title':'Student Registration Form',
+		'msgs':{
+			'success':'Thanks for submitting the form'
+		}
+	}
+}
